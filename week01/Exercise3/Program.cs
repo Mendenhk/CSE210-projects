fef83be9d6504d2c011489e590739313bed26220
Mendenhk <kris_mendenhall@yahoo.com>
2025-09-04 00:16:57 -0700

    Exercise 3 completion

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,8 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is the magic number?");
-        int guess = Console.ReadLine();
-        Console.Write($"{guess}");
+
+        int guess = 0;
+        int playAgain = -1;
+        int guesses = 0;
+
+        do
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 100); 
+            Console.WriteLine(number);       
+            guesses = 0;
+            do
+            {
+                Console.Write("What is your guess(1 to 100)? ");
+                guess = int.Parse(Console.ReadLine());
+                guesses++;
+                if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+            } while (guess != number);
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"It took you {guesses} tries to guess the number.");
+            Console.Write("Would you like to play again? Enter 1 for yes or 0 for no: ");
+            playAgain = int.Parse(Console.ReadLine());
+        } while (playAgain != 0);
+        Console.WriteLine("Thank you for playing.");
     }
 }
