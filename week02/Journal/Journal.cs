ef135f5c303fc495b57f5456226610cfb22eda61
Mendenhk <kris_mendenhall@yahoo.com>
2025-09-10 18:59:10 -0700

    Journal project initial stubbs/Classes

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,23 @@
+public class Journal
+{
+    //a list of class names
+    public List<Entry> _entries = new List<Entry>();
+
+
+    public void AddEntry(Entry newEntry)
+    {
+        Console.WriteLine("");
+    }
+    public void DisplayAll()
+    {
+        Console.WriteLine("");
+    }
+    public void SaveToFile(string file)
+    {
+        Console.WriteLine("");
+    }
+    public void LoadFromFile(string file)
+    {
+        Console.WriteLine("");
+    }
+}
