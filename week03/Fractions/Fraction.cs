ddd12371e6f1e9a893ff394701eca48f222b6fd8
Mendenhk <kris_mendenhall@yahoo.com>
2025-09-16 10:32:00 -0700

    week2 encapsulation assignment

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,59 @@
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    //The following three are what are called constructors (initialize objects with values)
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    // public int GetTop()
+    // {
+    //     return _top;
+    // }
+
+    // public void SetTop(int top)
+    // {
+    //     _top = top;
+    // }
+
+    // public int GetBottom()
+    // {
+    //     return _bottom;
+    // }
+
+    // public void SetBottom(int bottom)
+    // {
+    //     _bottom = bottom;
+    // }
+
+
+    public string GetFractionString()
+    {
+        string fractionString = $"{_top}/{_bottom}";
+        return fractionString;
+    }
+
+    public double GetDecimalValue()
+    {
+        //I had to convert either top or bottom to a double for the division to work correctly.
+        double decimalValue = (double)_top / (double)_bottom;
+        return decimalValue;
+    }
+
+}
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,27 @@
 using System;
 
+//my setters and initialization are not changing the values of top and bottom
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction newFraction = new Fraction();
+        // newFraction.SetTop(1);
+        // newFraction.SetBottom(3);
+        Console.WriteLine(newFraction.GetFractionString());
+        Console.WriteLine(newFraction.GetDecimalValue());
+
+        Fraction newFraction2 = new Fraction(5);
+        Console.WriteLine(newFraction2.GetFractionString());
+        Console.WriteLine(newFraction2.GetDecimalValue());
+
+        Fraction newFraction3 = new Fraction(3, 4);
+        Console.WriteLine(newFraction3.GetFractionString());
+        Console.WriteLine(newFraction3.GetDecimalValue());
+
+        Fraction newFraction4 = new Fraction(1, 3);
+        Console.WriteLine(newFraction4.GetFractionString());
+        Console.WriteLine(newFraction4.GetDecimalValue());
+
     }
 }
