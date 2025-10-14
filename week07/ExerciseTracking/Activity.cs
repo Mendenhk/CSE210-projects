4b6bb32c46937700ce13e5183b90e920f72dce3c
Mendenhk <kris_mendenhall@yahoo.com>
2025-10-14 16:46:18 -0700

    added comment

--- week07/ExerciseTracking/Activity.cs
+++ week07/ExerciseTracking/Activity.cs
@@ -24,12 +24,13 @@ public class Activity
 
     public virtual void GetPace()
     {
-        
+
     }
 
     //GetSummary method is defined in the base class and calls virtual methods for getting the distance, speed, and pace.
     //points docked for the following: GetSummary method is defined in the base class but it is overridden in each of the derived classes, rather than using a single method in the base class that does not need to be overridden.
-    public virtual string GetSummary(){
+    //Me: not virtual because will not be overridden
+    public string GetSummary(){
         return "hello";
     }
 }
